1ab2588ef3692497d9bbba166d5cc5ec8f6913ea
Alejandro Casna <alejandrocasna@hotmail.com>
2025-05-24 00:07:33 +0200

    vercel

---
+++ frontend/next.config.js
@@ -0,0 +1,6 @@
+// frontend/next.config.js
+module.exports = {
+  output: 'export',         // habilita `next export`
+  trailingSlash: true,      // opcional, para rutas /about/ en static
+  // si usas imágenes de Next Image en Vercel, comenta output: 'export'
+};
