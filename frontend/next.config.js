70d5c399fdf888a3d98633cebc28761bc58a38e6
Alejandro Casna <alejandrocasna@hotmail.com>
2025-05-26 12:23:58 +0200

    errores vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Para poder usar `next export`
-  output: 'export',
-  // Para rutas con slash al final
-  trailingSlash: true,
-  // Ignorar errores de ESLint que detengan el build
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Ignorar errores de TypeScript que detengan el build
   typescript: {
     ignoreBuildErrors: true,
   },
